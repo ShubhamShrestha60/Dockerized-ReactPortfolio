8112d0f9848f67c5b48e010896793e08866e0c36
Shubham Shrestha <shrestha.subham.60@gmail.com>
2025-01-03 18:59:37 +0545

    first commit

---
+++ shubham/backend/index.js
@@ -0,0 +1,13 @@
+// import express from "express";
+// const app = express ();
+
+
+
+// app.get("/", (req,res)=> {
+// res.send("Hello World!Shubhs");
+// });
+// app.listen(
+//     3000, ()=>{
+//         console.log("Server is running on port 5000")
+//     }
+);
---
+++ shubham/src/Constants/index.js
@@ -0,0 +1,200 @@
+import { aflacLogo, android, angular, cognizantLogo,
+     facebook, firebase, instagram, ionic, linkdin, mysql,
+      nodejs,
+      react, typescript, wellsLogo, wiuLogo, youtube } from "../assets"
+
+export const AppText={
+    hello:'Hello,',
+    Iam:'I am ',
+    ShubhamShrestha:'Shubham Shrestha',
+    aboutMeDescripion:'I am software developer working in IT industry from  last 6 Years as Full stack developer. Expert in UI/UX design, Front-end  and Back-end Development',
+    Skills:'Skills & ',
+    Experties:'Experties',
+    UIUXDesigner:'UI/UX Designer',
+    BackendDeveloper:'Backend Dev',
+    FrontedDeveloper:'Frontend Dev',
+    GraphiteArtist:'Graphite Artist',
+    Portfolio:'Portfolio',
+    Creative:'My Creative ',
+    CompaniesI:'Companies I ',
+    WorkedFor:'Worked For',
+    Contact:'Contact ',
+    Us:'Us',
+    copywriteText:'Copyright by Shubham Shrestha @2024'
+
+}
+
+export const aboutSection=[
+    {
+        id:1,
+        image:'https://img.freepik.com/premium-vector/ui-ux-programmer-flat-design-vector-illustration-business-information-team-sharing-ideas-with-designer-coding-interface-software-app-development_2175-1809.jpg?w=2000',
+        title:'UI/UX Designer',
+        desc:'Expert in UI/UX designing software like figma'
+    },
+    {
+        id:2,
+        image:'https://media.istockphoto.com/id/1304570729/vector/front-end-development-concept-vector-flat-graphic-design-illustration.jpg?s=612x612&w=0&k=20&c=0lpu0j-4FbuoA7xpGzt9apKZeI5F9KlNNe0qRYxjUYs=',
+        title:'Frontend Developer',
+        desc:'Expert in UI/UX designing software like figma'
+    },
+    {
+        id:3,
+        image:'https://img.freepik.com/premium-vector/back-end-developer-working-laptop_701961-1383.jpg?w=2000',
+        title:'Back-end Developer',
+        desc:'Expert in UI/UX designing software like figma'
+    }
+]
+
+export const skillsList=[
+    {
+        id:1,
+        icon:angular
+    },
+    {
+        id:1,
+        icon:react
+    },
+    {
+        id:1,
+        icon:typescript
+    },
+    {
+        id:1,
+        icon:firebase
+    },
+    {
+        id:1,
+        icon:android
+    },
+    {
+        id:1,
+        icon:ionic
+    },
+    {
+        id:1,
+        icon:mysql
+    },
+    {
+        id:1,
+        icon:react
+    },
+    {
+        id:1,
+        icon:typescript
+    },
+    {
+        id:1,
+        icon:firebase
+    },
+    {
+        id:1,
+        icon:android
+    },
+    {
+        id:1,
+        icon:nodejs
+    },
+
+]
+
+export const workDetail=[
+    { 
+        id:1,
+        year:'2021',
+        compnayName:'Opera Eye Wear',
+        position:'Sales Executive'
+    },
+    { 
+        id:1,
+        year:'2023',
+        compnayName:'Herald College Kathmandu',
+        position:'Business Development Intern'
+    },
+    // { 
+    //     id:1,
+    //     year:'2024',
+    //     compnayName:'Cloud Factory',
+    //     position:'2D Image Annotator'
+    // },
+    // { 
+    //     id:1,
+    //     year:'2024',
+    //     compnayName:'Freelance',
+    //     position:'Freelancing'
+    // },
+]
+
+export const portfolio=[
+    {
+        id:1,
+        title:'Real Estate UI/UX',
+        type:'ui/ux',
+        desc:"Ghar Jagga Dai complete real estate website design",
+        imageUrl:"https://cdn.dribbble.com/userupload/17409801/file/original-e77eab702bee68dba64f8878798cdb32.png?resize=1024x768&vertical=center"
+    },
+    {
+        id:4,
+        title:'Funny Website',
+        type:'website',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://miro.medium.com/max/1400/0*GWvV5kcuVATUfs2r.png"
+    },
+    {
+        id:2,
+        title:'Business Listing App',
+        type:'mobile',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://www.radiustheme.com/wp-content/uploads/2022/05/Directory-app.png"
+    },
+    {
+        id:3,
+        title:'Food Delivery UI/UX',
+        type:'ui/ux',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://cdn.dribbble.com/users/2892962/screenshots/14273462/nlytical_mockup_4x.png"
+    },
+
+    
+]
+
+export const CompanyImage=[
+    {
+        id:1,
+        imageUrl:cognizantLogo
+    },
+    {
+        id:1,
+        imageUrl:wellsLogo
+    },
+    {
+        id:1,
+        imageUrl:aflacLogo
+    },
+    {
+        id:1,
+        imageUrl:wiuLogo
+    }
+]
+
+export const socialNetwork=[
+    {
+        id:1,
+        logo:instagram,
+        url:'https://www.instagram.com/tubeguruji/'
+    },
+    {
+        id:1,
+        logo:youtube,
+        url:'https://www.youtube.com/channel/UC33Tc4FCgd_to314_b2hNTg',
+    },
+    {
+        id:1,
+        logo:linkdin,
+        url:'https://www.linkedin.com/in/rahul-sanap-652920108/'
+    },
+    {
+        id:1,
+        logo:facebook,
+        url:'https://www.facebook.com/rahul.sanap1'
+    },
+]
